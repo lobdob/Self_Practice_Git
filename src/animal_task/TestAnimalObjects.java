1e20b0ae29b0702f2a61adcf5a36bd8f18aeca27
elmirabayramova <elmirabayramova786@gmail.com>
2023-05-01 21:46:55 -0400

    created TestAnimalObjects

---
+++ src/animal_task/TestAnimalObjects.java
@@ -0,0 +1,17 @@
+package animal_task;
+
+public class TestAnimalObjects {
+    public static void main(String[] args) {
+        Cat cat = new Cat("Lucy", "Bengali", 'F');
+        System.out.println(cat);
+        cat.eat();
+
+        Dog dog = new Dog("Rex", "German Shepard", 'M');
+        System.out.println(dog);
+        dog.eat();
+
+        Fish fish=new Fish("Herman", "Salmon", 'M');
+        System.out.println(fish);
+        fish.eat();
+    }
+}
