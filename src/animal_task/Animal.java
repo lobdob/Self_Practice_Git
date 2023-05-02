d89b0dfcc62a1b3ad4fa8d789250e5791f9b21b2
elmirabayramova <elmirabayramova786@gmail.com>
2023-05-01 21:32:39 -0400

    created animal_package and animal class

---
+++ src/animal_task/Animal.java
@@ -0,0 +1,47 @@
+package animal_task;
+
+public abstract class Animal {
+    private String name;
+    private String breed;
+    private char gender;
+
+    public Animal(String name, String breed, char gender) {
+       setName(name);
+       setBreed(breed);
+       setGender(gender);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+
+    public char getGender() {
+        return gender;
+    }
+
+    public void setGender(char gender) {
+        this.gender = gender;
+    }
+    public abstract void eat();
+
+    @Override
+    public String toString() {
+        return getClass().getSimpleName()+"{" +
+                "name='" + name + '\'' +
+                ", breed='" + breed + '\'' +
+                ", gender=" + gender +
+                '}';
+    }
+}
